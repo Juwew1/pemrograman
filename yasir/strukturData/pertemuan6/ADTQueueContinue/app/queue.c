1fa030fad7e5de1e549848016b8a878555ac77f1
Yasir Noval <aibnu0371@gmail.com>
2024-03-19 08:12:02 +0700

    memulai pertemuan 6

---
+++ yasir/strukturData/pertemuan6/ADTQueueContinue/app/queue.c
@@ -0,0 +1,58 @@
+/**
+ * Program: queue.c
+ * Author: (2350081004, Muhamad Yasir Noval)
+ * Kelas: A
+ * Deskripsi: Header file dari prototype queue
+ * Tanggal: -
+ */
+
+#include "../lib/queue.h"
+
+#include <stdio.h>
+#include <conio.h>
+
+// Prototype Queue
+// Konstruktor membentuk Queue
+void CreateQueue(Queue * Q) {
+
+}
+
+// {Operasi terhadap komponen: selektor Get dan Set} : tidak perlu sudah di define diatas
+int NBElmt(Queue Q) {
+
+}
+
+// Destruktor/Dealokator: tidak perlu
+
+// {Kelompok operasi pada Queue}
+boolean IsQueueEmpty(Queue Q) {
+
+}
+
+boolean IsQueueFull(Queue Q) {
+
+}
+
+// Menambahkan sebuah element ke queue
+void AddQueue(Queue *Q, infotype X) {
+
+}
+
+// Menghapus sebuah element queue
+void DelQueue(Queue *Q, infotype *X) {
+
+}
+
+// {Kelompok interaksi dengan i/o device, Baca/Tulis}
+void PrintQueueInfo(Queue S) {
+
+}
+
+// Kelompok operasi lain terhadap type
+boolean isInfoKetemu(Queue S, infotype X) {
+
+}
+
+address CariElemenQueue(Queue Q, int X) {
+
+}
---
+++ yasir/strukturData/pertemuan6/ADTQueueContinue/main.c
@@ -0,0 +1,22 @@
+/**
+ * Program: main.c
+ * Author: (2350081004, Muhamad Yasir Noval)
+ * Kelas: A
+ * Deskripsi: Header file dari prototype queue
+ * Tanggal: -
+ */
+
+#include "app/queue.c"
+
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    // Kamus
+    Queue MyTab;
+
+    // Algoritma
+    CreateQueue(&MyTab);
+
+    return 0;
+}
